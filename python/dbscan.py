2b0e2a979b7e2a6d09b36b75602ee96a689b6d30
Barna Gabor <mr.barna.gabor@gmail.com>
2014-10-31 17:26:05 +0100

    move files

--- python/dbscan.py
+++ python/dbscan.py
@@ -15,13 +15,13 @@ class DbscanModel(object):
 
 class Dbscan(object):
     @classmethod
-    def train(cls, rdd):
+    def train(cls, rdd, epsilon, numOfPoints):
         sc = rdd.context
         jrdd = _get_unmangled_double_vector_rdd(rdd)._jrdd
-        model = sc._jvm.PythonDbscanAPI().train(jrdd, 25, 30)
+        model = sc._jvm.PythonDbscanAPI().train(jrdd, epsilon, numOfPoints)
         return DbscanModel(model)
 
 
 if __name__ == '__main__':
-    model = Dbscan.train(sc.parallelize([Vectors.sparse(4, {1: 1.0, 3: 5.5})]))
+    model = Dbscan.train(sc.parallelize([Vectors.sparse(4, {1: 1.0, 3: 5.5})]), 25, 30)
     print model.predict(Vectors.sparse(4, {1: 1.0, 3: 5.5}))
